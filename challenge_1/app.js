51ca7d8547e83501fba888e9abe3abde59b01911
hanialazreg <lazreghania@gmail.com>
2019-12-27 14:45:46 +0100

    Initial Board Setup

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,34 @@
 console.log('file setup')
 
+document.getElementById("11").addEventListener("click", function insertText () {
+  document.getElementById('11').innerHTML = "X";});
+
+
+
+document.getElementById("12").addEventListener("click", function insertText () {
+  document.getElementById('12').innerHTML = "X";});
+
+document.getElementById("13").addEventListener("click", function insertText () {
+  document.getElementById('13').innerHTML = "X";});
+
+document.getElementById("21").addEventListener("click", function insertText () {
+  document.getElementById('21').innerHTML = "X";});
+
+document.getElementById("22").addEventListener("click", function insertText () {
+  document.getElementById('22').innerHTML = "X";});
+
+document.getElementById("23").addEventListener("click", function insertText () {
+  document.getElementById('23').innerHTML = "X";});
+
+document.getElementById("31").addEventListener("click", function insertText () {
+  document.getElementById('31').innerHTML = "X";});
+
+document.getElementById("32").addEventListener("click", function insertText () {
+  document.getElementById('32').innerHTML = "X";});
+
+document.getElementById("33").addEventListener("click", function insertText () {
+  document.getElementById('33').innerHTML = "X";});
+
+
+
+document.getElementById("btn1").addEventListener("click", function(){alert("this is button for reset ")});
